94280f8b48f4a0759b80ea345c74e9fcd404c3a5
ZEDO B <zedoaboda@gmail.com>
2024-03-05 18:42:05 +0200

    - Make some fixes

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(PointsCounter());
+  runApp(const PointsCounter());
 }
 
 
 
 class PointsCounter extends StatelessWidget {
+  const PointsCounter({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (BuildContext context) => CounterCubit(),
-      child: MaterialApp(
+      child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Home(),
       ),
@@ -24,7 +26,7 @@ class PointsCounter extends StatelessWidget {
 }
 
 class Home extends StatefulWidget {
-  Home({
+  const Home({
     Key? key,
   }) : super(key: key);
 
